809196bac8c657e883da34a4b28a852560dda598
윤준석 <y2o2u2n@gmail.com>
2020-01-16 20:12:34 +0900

    Initial commit

---
+++ src/main/java/com/y2o2u2n/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.y2o2u2n.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/y2o2u2n/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y2o2u2n.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
