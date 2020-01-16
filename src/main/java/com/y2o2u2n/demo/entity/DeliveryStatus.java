8acd78d9893fd70d7ed8a62ac213aee8d43ba9f4
윤준석 <y2o2u2n@gmail.com>
2020-01-16 20:31:42 +0900

    도메인 분석 설계

---
+++ src/main/java/com/y2o2u2n/demo/entity/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.y2o2u2n.demo.entity;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ src/main/java/com/y2o2u2n/demo/entity/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.y2o2u2n.demo.entity;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
